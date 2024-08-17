41a3ff38740387979175264f9436bef9b2194113
temez <66576832+temiolu@users.noreply.github.com>
2024-08-17 17:25:55 -0500

    Delete LSSF Room Occupancy Application directory

--- LSSF Room Occupancy Application/backend/creds.py
+++
@@ -1,6 +0,0 @@
-class Creds:
-    def __init__(self):
-        self.conString = "cis3368fall.ciantlto98wq.us-east-1.rds.amazonaws.com"
-        self.userName = "admin"
-        self.password = "cis3368farfall"
-        self.dbName = "cis3368falldb"
